aa42836a9387f21222ab2122ab5c79a7660e85b1
samuelmorenov <xamelsamuel@gmail.com>
2021-02-12 18:03:01 +0100

    Uso de numpy para manejo de la matriz de tablero y simplificacion del metodo robarCarta

--- src/Controller.py
+++ src/Controller.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 from random import randrange
 
 PLAYER = 1
@@ -7,11 +8,8 @@ COMPUTER = 2
 
 class Controller:
     def __init__(self):
-        self.mazo = []
         self.initMazo()
-        
-        self.tablero = {}
-        self.initTablero()
+        self.tablero = np.zeros((7,7), dtype=int)
                     
     def initMazo(self):
         self.mazo = [1,1,2,2,3,3,4,4,4,5,5,5,6,6,6,6,7,7,7,7,7]
@@ -20,13 +18,5 @@ class Controller:
     def robarCarta(self):
         if(len(self.mazo) < 1):
             raise Exception("Mazo vacio")
-        numeroGenerado = randrange(len(self.mazo))
-        carta = self.mazo.pop(numeroGenerado)
-        return carta
-    
-    def initTablero(self):
-        for i in range(1,7):
-            for j in range(1,7):
-                self.tablero[(i,j)] = 0
+        return self.mazo.pop(randrange(len(self.mazo)))
                 
-    
