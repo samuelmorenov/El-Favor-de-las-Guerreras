e0388a9f21eab91949b554bf132c4526449d9d28
samuelmorenov <xamelsamuel@gmail.com>
2021-03-28 17:49:17 +0200

    Añadida tabla de botones a la interfaz

--- src/interface/GUI_Tkinter.py
+++ src/interface/GUI_Tkinter.py
@@ -5,11 +5,29 @@ sys.path.append('../')
 
 from tkinter import *
 
+import controller.Constantes as const
+
 class GUI_Tkinter:
     def __init__(self):
-        self.window = Tk();
+        self.window = Tk()
+        self.printTable()
+        self.start()
+        return
+    
+    def printTable(self):
+        for f in range(const.NFILA):
+            for c in range(const.NCOLUMNA):
+                text = str(f)+","+str(c)
+                ButtonToAdd = Button(self.window, text = text)
+                ButtonToAdd.grid(row=f, column=c)
+            
+        
         return
     
     def start(self):
         self.window.mainloop()
         
+        
+        
+if __name__ == "__main__":
+    tkinter = GUI_Tkinter()
