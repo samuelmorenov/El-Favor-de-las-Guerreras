3a482bd3e25d962bca0dc1b7b7c4238a0bdcaaf4
samuelmorenov <xamelsamuel@gmail.com>
2021-04-01 20:11:10 +0200

    Añadida nuevo archivo de paths de imagenes y retocado tamaño de botones

--- src/interface/GUI_Tkinter.py
+++ src/interface/GUI_Tkinter.py
@@ -7,8 +7,8 @@ from tkinter import Tk, Button, Label, DISABLED
 from PIL import Image, ImageTk
 
 import controller.Constantes as const
+import interface.ImagesPath as ip
 
-static = "static/"
 bgcolor = '#c4a495'
 
 class GUI_Tkinter:
@@ -48,16 +48,16 @@ class GUI_Tkinter:
     
     def __addFila(self, finaIndice, filaTablero):
         for c in range(const.NCOLUMNA):
-            self.__addButton(finaIndice, c, filaTablero[c])
+            self.__addBasicButton(finaIndice, c, filaTablero[c])
             
     def __addGuerreras(self, fila):
-        self.__addCarta(fila, 0, 'guerrera1.png')
-        self.__addCarta(fila, 1, 'guerrera2.png')
-        self.__addCarta(fila, 2, 'guerrera3.png')
-        self.__addCarta(fila, 3, 'guerrera4.png')
-        self.__addCarta(fila, 4, 'guerrera5.png')
-        self.__addCarta(fila, 5, 'guerrera6.png')
-        self.__addCarta(fila, 6, 'guerrera7.png')
+        self.__addCartaGrande(fila, 0, ip.G1)
+        self.__addCartaGrande(fila, 1, ip.G2)
+        self.__addCartaGrande(fila, 2, ip.G3)
+        self.__addCartaGrande(fila, 3, ip.G4)
+        self.__addCartaGrande(fila, 4, ip.G5)
+        self.__addCartaGrande(fila, 5, ip.G6)
+        self.__addCartaGrande(fila, 6, ip.G7)
         
     def __addSusAcciones(self, fila, acciones):
         self.__addAccion(fila, 0, acciones[const.TIPO_SECRETO])
@@ -67,10 +67,11 @@ class GUI_Tkinter:
         
     def __addMisAcciones(self, fila, acciones):
         self.__addAccion(fila, 0, acciones[const.TIPO_SECRETO])
-        self.__addButton(fila, 1, acciones[const.TIPO_SECRETO])
+        self.__addBasicButton(fila, 1, acciones[const.TIPO_SECRETO])
+        
         self.__addAccion(fila, 2, acciones[const.TIPO_RENUNCIA])
-        self.__addButton(fila, 3, acciones[const.TIPO_RENUNCIA_1])
-        self.__addButton(fila, 4, acciones[const.TIPO_RENUNCIA_2])
+        self.__addBasicButton(fila, 3, acciones[const.TIPO_RENUNCIA_1])
+        self.__addBasicButton(fila, 4, acciones[const.TIPO_RENUNCIA_2])
         self.__addAccion(fila, 5, acciones[const.TIPO_REGALO])
         self.__addAccion(fila, 6, acciones[const.TIPO_COMPETICION])
         
@@ -80,37 +81,49 @@ class GUI_Tkinter:
     Metodos para añadir objetos a las filas
     '''
     def __addAccion(self, fila, columna, valor):
-        activo = 'azul.png'
-        inactivo = 'azul_gris.png'
         if(valor == 0):
-            self.__addLabelConImagen(fila, columna, inactivo, const.CARTA_ANCHO, const.CARTA_ANCHO)
+            self.__addLabelConImagen(fila, columna, ip.INACTIVO, const.CARTA_ANCHO, const.CARTA_ANCHO)
         else:
-            self.__addLabelConImagen(fila, columna, activo, const.CARTA_ANCHO, const.CARTA_ANCHO)
+            self.__addLabelConImagen(fila, columna, ip.ACTIVO, const.CARTA_ANCHO, const.CARTA_ANCHO)
             
-    def __addCarta(self, fila, columna, path):
+    def __addCartaGrande(self, fila, columna, path):
         self.__addLabelConImagen(fila, columna, path, const.CARTA_ALTO, const.CARTA_ANCHO)
         
-    def __addLabelConImagen(self, fila, columna, path, alto, ancho):
-        image_path = static+path
-
+        
+        
+        
+    def __addLabelConImagen(self, fila, columna, image_path, alto, ancho):
+        
         image = Image.open(image_path)
         image = image.resize((ancho, alto), Image.ANTIALIAS)
         photo = ImageTk.PhotoImage(image)
-        label = Label(self.__window, image = photo, width=ancho, height=alto, borderwidth=0, bg=bgcolor)
+        label = Label(self.__window,
+                      image = photo,
+                      width=ancho,
+                      height=alto,
+                      bg=bgcolor)
         label.image = photo
-        label.grid(row=fila, column=columna)
+        label.grid(row=fila, column=columna, padx=const.PADDING, pady=const.PADDING)
+        
+    def __addBasicButton(self, fila, columna, text):
+        self.__addButton(fila, columna, const.BOTON_ALTO, const.BOTON_ANCHO, DISABLED, text)
+        
+    def __addButton(self, fila, columna, alto, ancho, estado, text):
+        boton = Button(
+                self.__window,
+                text = text,
+                state=estado,
+                borderwidth=const.BOTON_BORDE, 
+                bg=bgcolor)
+        boton.grid(row=fila, column=columna, padx=const.PADDING, pady=const.PADDING)
         
     def __addButtonConFondo(self, fila, columna, color):
         #butn = Button(frm, text="Welcome", fg="pink",activebackground = "green")
         return
     
     def __addHueco(self, fila, columna):
-        label = Label(self.__window, text = " ", borderwidth=0, bg=bgcolor)
+        label = Label(self.__window, bg=bgcolor)
         label.grid(row=fila, column=columna)
-        
-    def __addButton(self, fila, columna, text):
-        ButtonToAdd = Button(self.__window, text = text, state=DISABLED, borderwidth=0, bg=bgcolor)
-        ButtonToAdd.grid(row=fila, column=columna)
         
     '''
     Metodos de control de loop
@@ -118,7 +131,7 @@ class GUI_Tkinter:
     
     def __printAceptar(self):
         ButtonToAdd = Button(self.__window, text = "Aceptar", command = self.__pressAceptar)
-        ButtonToAdd.grid(row=const.NFILA-1, column=const.NCOLUMNA+1)
+        ButtonToAdd.grid(row=const.NFILA-1, column=int(const.NCOLUMNA/2))
         
     def start(self):
         self.__window.mainloop()
---
+++ src/interface/ImagesPath.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+static = "static/"
+
+G1 = static+"guerrera1.png"
+G2 = static+"guerrera2.png"
+G3 = static+"guerrera3.png"
+G4 = static+"guerrera4.png"
+G5 = static+"guerrera5.png"
+G6 = static+"guerrera6.png"
+G7 = static+"guerrera7.png"
+
+C1 = static+"carta1.png"
+C2 = static+"carta2.png"
+C3 = static+"carta3.png"
+C4 = static+"carta4.png"
+C5 = static+"carta5.png"
+C6 = static+"carta6.png"
+C7 = static+"carta7.png"
+
+ACTIVO = static+"azul.png"
+INACTIVO = static+"azul_gris.png"
