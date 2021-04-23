97eb1183401db5d2602f74097b34d19137179b87
samuelmorenov <xamelsamuel@gmail.com>
2021-04-23 21:51:17 +0200

    Movido modo generar datos a un metodo separado en el main

--- src/main.py
+++ src/main.py
@@ -9,31 +9,35 @@ from controller.PartidaController import PartidaController
 
 import controller.Constantes as const
 
+def modo_generar_datos():
+    partidasGanadas1 = 0
+    partidasGanadas2 = 0
+    
+    dir='./../data'
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    path = dir + "/jugadasGanadoras.csv"
+    with open(path, 'w') as f:
+        f.write('')
+    
+    for i in range(const.NUM_SIMULACIONES):
+        mainController = PartidaController('generacion')
+        mainController.start()
+        if(mainController.winner == const.JUGADOR1):
+            partidasGanadas1 = partidasGanadas1 + 1
+        else:
+            partidasGanadas2 = partidasGanadas2 + 1
+            
+        print("Partidas ganadas por el 1: "+ str(partidasGanadas1))
+        print("Partidas ganadas por el 2: "+ str(partidasGanadas2))
+        print()
+
 
 if __name__ == "__main__":
     if(const.MODO == const.MODO_GENERAR_DATOS):
-        partidasGanadas1 = 0
-        partidasGanadas2 = 0
-        
-        dir='./../data'
-        if not os.path.exists(dir):
-            os.mkdir(dir)
-        path = dir + "/jugadasGanadoras.csv"
-        with open(path, 'w') as f:
-            f.write('')
-        
-        for i in range(const.NUM_SIMULACIONES):
-            mainController = PartidaController('generacion')
-            mainController.start()
-            if(mainController.winner == const.JUGADOR1):
-                partidasGanadas1 = partidasGanadas1 + 1
-            else:
-                partidasGanadas2 = partidasGanadas2 + 1
-                
-            print("Partidas ganadas por el 1: "+ str(partidasGanadas1))
-            print("Partidas ganadas por el 2: "+ str(partidasGanadas2))
-            print()
+        modo_generar_datos()
     
     if(const.MODO == const.MODO_JUGAR):
         mainController = PartidaController('jugar')
         mainController.start()
+
