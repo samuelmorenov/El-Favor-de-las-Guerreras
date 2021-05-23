b8c5e20ba7be29da58d05047fd36e231614a97ee
samuelmorenov <xamelsamuel@gmail.com>
2021-05-23 17:23:43 +0200

    Modificada clase de entrenamiento para tener metodos y atributos privados

--- src/training/Entrenamiento.py
+++ src/training/Entrenamiento.py
@@ -34,16 +34,16 @@ class Entrenamiento:
         #Eliminamos sesiones de keras abiertas
         K.clear_session()
         
-        entrada, salida = self.preProcesadoDeDatos()
+        entrada, salida = self.__preProcesadoDeDatos()
         #print(entrada)
         #print(salida)
         
-        self.cnn = None
-        self.creacionModelo()
-        self.complileAndFit(entrada, salida)
-        self.guardarModelo()
+        self.__cnn = None
+        self.__creacionModelo()
+        self.__complileAndFit(entrada, salida)
+        self.__guardarModelo()
         
-    def preProcesadoDeDatos(self):
+    def __preProcesadoDeDatos(self):
         tipos = int
         cabecera = None
         separador = data.SEPARADOR
@@ -67,9 +67,9 @@ class Entrenamiento:
         
         return training_entrada, training_salida
     
-    def establecerCapas(self):
+    def __establecerCapas(self):
         #Primera capa de convolucion
-        self.cnn.add(Convolution2D( 
+        self.__cnn.add(Convolution2D( 
                 PNN.filtrosConv1, 
                 PNN.tamanio_filtro1, 
                 padding='same', #lo que va a hacer el filtro en las esquinas
@@ -78,12 +78,12 @@ class Entrenamiento:
                 ))
         
         #Primera capa de pooling
-        self.cnn.add(MaxPooling2D(
+        self.__cnn.add(MaxPooling2D(
                 pool_size=PNN.tamanio_pool
                 ))
         
         #Siguiente capa de convolucion
-        self.cnn.add(Convolution2D( 
+        self.__cnn.add(Convolution2D( 
                 PNN.filtrosConv2, 
                 PNN.tamanio_filtro2, 
                 padding='same', #lo que va a hacer el filtro en las esquinas
@@ -91,42 +91,42 @@ class Entrenamiento:
                 ))
         
         #Siguiente capa de pooling
-        self.cnn.add(MaxPooling2D(
+        self.__cnn.add(MaxPooling2D(
                 pool_size=PNN.tamanio_pool
                 ))
         
         #Transformacion de la red en una dimension
-        self.cnn.add(Flatten())
+        self.__cnn.add(Flatten())
         
         #Capa densa
-        self.cnn.add(Dense(
+        self.__cnn.add(Dense(
                 256, #numero de neuronas
                 activation='relu'
                 ))
         
         #Se apagaran el 50% de las neuronas para no atrofiar caminos
-        self.cnn.add(Dropout(0.5)) 
+        self.__cnn.add(Dropout(0.5)) 
         
         #Ultima capa
-        self.cnn.add(Dense(
+        self.__cnn.add(Dense(
                 5, #numero de neuronas de salida
                 activation='softmax' #% de cada opcion
                 ))
     
     
-    def creacionModelo(self):
-        self.cnn = tf.keras.Sequential([layers.Dense(64), layers.Dense(1)])
+    def __creacionModelo(self):
+        self.__cnn = tf.keras.Sequential([layers.Dense(64), layers.Dense(1)])
         
         
-    def complileAndFit(self, entrada, salida):
-        self.cnn.compile(loss = tf.losses.MeanSquaredError(),optimizer = tf.optimizers.Adam())
-        self.cnn.fit(entrada, salida, epochs=10)
+    def __complileAndFit(self, entrada, salida):
+        self.__cnn.compile(loss = tf.losses.MeanSquaredError(),optimizer = tf.optimizers.Adam())
+        self.__cnn.fit(entrada, salida, epochs=10)
         
-    def guardarModelo(self):
+    def __guardarModelo(self):
         dir=data.MODELO_DIR
 
         if not os.path.exists(dir):
             os.mkdir(dir)
             
-        self.cnn.save(data.MODELO_NOMBRE) #guardado del modelo
-        self.cnn.save_weights(data.MODELO_PESOS) #guardado de los pesos del modelo
+        self.__cnn.save(data.MODELO_NOMBRE) #guardado del modelo
+        self.__cnn.save_weights(data.MODELO_PESOS) #guardado de los pesos del modelo
