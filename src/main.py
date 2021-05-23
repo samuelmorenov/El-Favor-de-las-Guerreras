9430fb3082da420ea253101940aa986c3c9ba606
samuelmorenov <xamelsamuel@gmail.com>
2021-05-23 16:24:29 +0200

    Renombrada carpeta de entrenamiento

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from controller.PartidaController import PartidaController
-from neuralNetwork.entrenamiento import Entrenamiento
+from training.Entrenamiento import Entrenamiento
 from controller.DataGeneratorController import DataGeneratorController
 
 import parameterization.ParametrosMenu as menu
