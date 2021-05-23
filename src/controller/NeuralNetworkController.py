607372c0c14dbfa0013be3563c84efdf805e9d00
samuelmorenov <xamelsamuel@gmail.com>
2021-05-23 18:48:08 +0200

    Añadido imprimir traza al controller de la red neuronal

--- src/controller/NeuralNetworkController.py
+++ src/controller/NeuralNetworkController.py
@@ -3,6 +3,9 @@
 import sys
 sys.path.append('../')
 
+import parameterization.ParametrosTablero as const
+import parameterization.ParametrosMenu as menu
+
 from training.Prediccion import Prediccion
 
 class NeuralNetworkController:
@@ -18,7 +21,26 @@ class NeuralNetworkController:
         return self.__pedirAccion(tablero)
     
     def __pedirAccion(self, tablero):
-        return self.Prediccion.predecir(tablero)
+        if(menu.PRINT_TRACE):
+            if(self.miNumero == const.JUGADOR1):
+                print("\033[;33m",end="") #Amarillo
+            if(self.miNumero == const.JUGADOR2):
+                print("\033[;36m",end="") #Cian
+            
+            print("Soy "+self.miNombre)
+            
+            print("- Este es el tablero que me llega:")
+            print(tablero)
+            
+        accion = self.Prediccion.predecir(tablero)
+    
+        if(menu.PRINT_TRACE):
+            print("- Esta es la accion que realizo:")
+            print(accion)
+            print("\033[0m",end="")
+            print("___________________________________") #Separador de bots
+            
+        return accion
     
     def finish(self):
         return
