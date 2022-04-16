7f8c529ec2c0fbde6d69b19bb5846cf8b070619c
samuelmorenov <xamelsamuel@gmail.com>
2022-04-16 14:05:36 +0200

    Añadidos test de seleccionar accion para el bot

--- src/test/Tests.py
+++ src/test/Tests.py
@@ -5,27 +5,129 @@ import unittest
 import sys
 sys.path.append('../')
 
-from controller.BotTonto import BotTonto
+#import parameterization.ParametrosTablero as const
+
+from main.python.controller.BotTonto import BotTonto
 from Utils import Utils
 
 utils = Utils() 
 
-tablero = [[1,2,4,5,5,6,7],
-           [0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0],
-           [1,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0]]
+accionesDisponiblesTodas            = [0,0,0,0,0,0,0]
+accionesDisponiblesSecreto          = [0,2,3,1,1,0,0]
+accionesDisponiblesRenuncia         = [2,0,0,1,1,0,0]
+accionesDisponiblesRegalo           = [2,3,4,0,1,0,0]
+accionesDisponiblesCompeticion      = [2,3,4,1,0,0,0]
+accionesDisponiblesNinguna          = [2,3,4,0,1,1,0]
+
+mano7CartasDistintas = [1,2,3,4,5,6,7]
+mano4Iguales = [7,7,7,7,0,0,0]
+mano3Iguales = [7,7,7,0,0,0,0]
+mano2Iguales = [7,7,0,0,0,0,0]
+mano1Iguales = [7,0,0,0,0,0,0]
+manoVacia = [7,0,0,0,0,0,0]
 
 
 class Test(unittest.TestCase):
     
-    def test_bot_accionCorrecta(self):
-        bot = BotTonto("test", 0)
-        accion = bot.decidirAccion(tablero)
-        utils.accionCorrecta(tablero, accion)
+    def __init__(self, *args, **kwargs):
+        super(Test, self).__init__(*args, **kwargs)
+        self.bot = BotTonto("Bot de testing", 0)
+    
+    '''    
+    Prueba del bot, seleccion de accion con:
+        4 acciones libres
+        7 cartas distintas a elegir
+    '''
+    def test_caso_1(self):
+        utils.accionCorrecta(self.bot, mano7CartasDistintas, accionesDisponiblesTodas)
+        
+    '''    
+    Prueba del bot, seleccion de accion con:
+        4 acciones libres
+        4 cartas iguales a elegir
+    '''
+    def test_caso_2(self):
+        utils.accionCorrecta(self.bot, mano4Iguales, accionesDisponiblesTodas)
+        
+    '''    
+    Prueba del bot, seleccion de accion con:
+        4 acciones libres
+        0 cartas a elegir        
+    '''
+    def test_caso_3(self):
+        utils.accionException(self.bot, manoVacia, accionesDisponiblesTodas)
+
+    '''    
+    Prueba del bot, seleccion de accion con:
+        Accion libre de tipo Secreto
+        7 cartas distintas a elegir        
+    '''
+    def test_caso_4(self):
+        utils.accionCorrecta(self.bot, mano7CartasDistintas, accionesDisponiblesSecreto)
+
+    '''    
+    Prueba del bot, seleccion de accion con:
+        Accion libre de tipo Secreto
+        1 carta a elegir
+    '''
+    def test_caso_5(self):
+        utils.accionCorrecta(self.bot, mano1Iguales, accionesDisponiblesSecreto)
+
+    '''    
+    Prueba del bot, seleccion de accion con:
+        Accion libre de tipo Renuncia
+        7 cartas distintas a elegir
+    '''
+    def test_caso_6(self):
+        utils.accionCorrecta(self.bot, mano7CartasDistintas, accionesDisponiblesRenuncia)
+
+    '''    
+    Prueba del bot, seleccion de accion con:
+        Accion libre de tipo Renuncia
+        2 de cartas a elegir, todas iguales
+    '''
+    def test_caso_7(self):
+        utils.accionCorrecta(self.bot, mano2Iguales, accionesDisponiblesRenuncia)
+
+    '''    
+    Prueba del bot, seleccion de accion con:
+        Accion libre de tipo Regalo
+        7 cartas distintas a elegir
+    '''
+    def test_caso_8(self):
+        utils.accionCorrecta(self.bot, mano7CartasDistintas, accionesDisponiblesRegalo)
+
+    '''    
+    Prueba del bot, seleccion de accion con:
+        Accion libre de tipo Regalo
+        3 de cartas a elegir, todas iguales
+    '''
+    def test_caso_9(self):
+        utils.accionCorrecta(self.bot, mano3Iguales, accionesDisponiblesRegalo)
+
+    '''    
+    Prueba del bot, seleccion de accion con:
+        Accion libre de tipo Competicion
+        7 cartas distintas a elegir
+    '''
+    def test_caso_10(self):
+        utils.accionCorrecta(self.bot, mano7CartasDistintas, accionesDisponiblesCompeticion)
+
+    '''    
+    Prueba del bot, seleccion de accion con:
+        Accion libre de tipo Competicion
+        4 de cartas a elegir, todas iguales
+    '''
+    def test_caso_11(self):
+        utils.accionCorrecta(self.bot, mano4Iguales, accionesDisponiblesCompeticion)
+
+    '''    
+    Prueba del bot, seleccion de accion con:
+        Sin acciones libres
+    '''
+    def test_caso_12(self):
+        utils.accionException(self.bot, mano7CartasDistintas, accionesDisponiblesNinguna)
+
         
 if __name__ == "__main__":
     unittest.main()
--- src/test/Utils.py
+++ src/test/Utils.py
@@ -5,16 +5,56 @@ import unittest
 import sys
 sys.path.append('../')
 
+import numpy as np
+
 import parameterization.ParametrosTablero as const
+
+tableroVacio = [[0,0,0,0,0,0,0], #MANO_JUGADOR1
+               [0,0,0,0,0,0,0], #MANO_JUGADOR2
+               [0,0,0,0,0,0,0], #ACCIONES_USADAS_JUGADOR1
+               [0,0,0,0,0,0,0], #ACCIONES_USADAS_JUGADOR2
+               [0,0,0,0,0,0,0], #ARMAS_USADAS_JUGADOR1
+               [0,0,0,0,0,0,0], #ARMAS_USADAS_JUGADOR2
+               [0,0,0,0,0,0,0], #FAVOR_DE_GUERRERA
+               [0,0,0,0,0,0,0]] #ACCION_PENDIENTE
 
 class Utils(unittest.TestCase):
     
-    def accionCorrecta(self, tablero, accion):
+    
+    def accionCorrecta(self, actor, mano, accionesDisponibles):
+        tablero = tableroVacio
+        tablero[const.ACCIONES_USADAS_JUGADOR1] = accionesDisponibles
+        tablero[const.MANO_JUGADOR1] = mano
+        
+        #Realización de accion a probar
+        accion = actor.decidirAccion(tablero)
+        
+        #Comprobación de datos obtenidos
+        #LA accion esta dentro de las posibles
+        self.accionPosible(accion, accionesDisponibles)
+        #La accion tiene un numero de cartas correcto
+        numero = self.accionNumeroCartasCorrecto(accion)
+        #La accion usa cartas disponibles en la mano
+        self.accionCartasDisponibles(accion, mano, numero)
+        
+    def accionException(self, actor, mano, accionesDisponibles):
+        tablero = tableroVacio
+        tablero[const.ACCIONES_USADAS_JUGADOR1] = accionesDisponibles
+        tablero[const.MANO_JUGADOR1] = mano
+        
+        
+        self.assertRaises(actor.decidirAccion(tablero))
+    
+    '''
+        Comprueba que la accion esta dentro de las acciones no realizadas
+        accion: es el array de la accion completa realizada
+        accionesRealizadas: es el array del estado de las acciones realizadas
+    '''
+    def accionPosible(self, accion, accionesRealizadas):
         listaAccionesPosibles = []
-        accionesRealizadas = tablero[const.ACCIONES_USADAS_JUGADOR1]
         if(accionesRealizadas[const.TIPO_SECRETO] == 0):
             listaAccionesPosibles.append(const.TIPO_SECRETO)
-        if(accionesRealizadas[const.TIPO_RENUNCIA_1] == 0):
+        if(accionesRealizadas[const.TIPO_RENUNCIA] == 0):
             listaAccionesPosibles.append(const.TIPO_RENUNCIA)
         if(accionesRealizadas[const.TIPO_REGALO] == 0):
             listaAccionesPosibles.append(const.TIPO_REGALO)
@@ -22,3 +62,62 @@ class Utils(unittest.TestCase):
             listaAccionesPosibles.append(const.TIPO_COMPETICION)
             
         self.assertIn(accion[const.ACCION_REALIZADA], listaAccionesPosibles)
+        
+    '''
+        Comprueba que el numero de cartas seleccionado corresponde con la accion
+        accion: es el array de la accion completa realizada
+    '''
+    def accionNumeroCartasCorrecto(self, accion):
+        num = self.__getNumeroCartasSeleccionadas(accion)
+        self.assertNotEqual(num, 0) #Comprobamos que no es 0
+        
+        numObjetivo = 0
+        
+        if(accion[const.ACCION_REALIZADA] == const.TIPO_SECRETO):
+            numObjetivo = const.ACCION_1_COUNT
+        if(accion[const.ACCION_REALIZADA] == const.TIPO_RENUNCIA):
+            numObjetivo = const.ACCION_2_COUNT
+        if(accion[const.ACCION_REALIZADA] == const.TIPO_REGALO):
+            numObjetivo = const.ACCION_3_COUNT
+        if(accion[const.ACCION_REALIZADA] == const.TIPO_COMPETICION):
+            numObjetivo = const.ACCION_4_COUNT
+        
+        self.assertEqual(num, numObjetivo)
+        
+        return numObjetivo
+    
+    '''
+        Comprueba que las cartas de la accion estan en la mano
+        accion: es el array de la accion completa realizada
+        mano: es el array de las cartas que hay en mano
+        numero: es el numero de cartas que tiene la accion
+    '''
+    def accionCartasDisponibles(self, accion, mano, numero):
+        manoRestante = mano
+        for i in range(1, numero+1, 1):
+            carta = accion[i]
+            self.assertIn(carta, manoRestante)
+            manoRestante = self.__eliminarCarta(manoRestante, carta)
+            
+        
+    def __getNumeroCartasSeleccionadas(self, accion):
+        num = 0
+        empiezanLosCeros = False
+        error = False
+        for i in range(1, len(accion), 1):
+            if((accion[i] != 0) & (not empiezanLosCeros)):
+                num = num + 1
+            elif((accion[i] == 0) & (not empiezanLosCeros)):
+                empiezanLosCeros = True
+            elif((accion[i] != 0) & empiezanLosCeros):
+                error = True
+                break
+            elif((accion[i] == 0) & empiezanLosCeros):
+                continue
+        if(error):
+            return 0
+        else:
+            return num
+        
+    def __eliminarCarta(self, lista, valor):
+        return np.delete(lista, np.argwhere(lista == valor)[0])
