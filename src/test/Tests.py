635eddb1b4cb1ec4184a2e39c1c6b09b344e802c
samuelmorenov <xamelsamuel@gmail.com>
2022-05-01 20:49:05 +0200

    Añadidas pruebas de la red neuronal

--- src/test/Tests.py
+++ src/test/Tests.py
@@ -8,6 +8,7 @@ sys.path.append('../')
 #import parameterization.ParametrosTablero as const
 
 from main.python.controller.BotTonto import BotTonto
+from main.python.controller.NeuralNetworkController import NeuralNetworkController
 from Utils import Utils
 
 utils = Utils() 
@@ -38,6 +39,7 @@ class Test(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(Test, self).__init__(*args, **kwargs)
         self.bot = BotTonto("Bot de testing", 0)
+        self.redNeuronal = NeuralNetworkController("Red neuronal de testing", 0)
         
     #-----------------------Bot Seleccionar accion-----------------------   
     #Prueba del bot, seleccion de accion con:
@@ -136,6 +138,104 @@ class Test(unittest.TestCase):
     #   Las dos opciones distintas
     def test_caso_17(self):
         utils.accionSeleccionCorrecta(self.bot, cartasAccionSeleccionCompeticionDistintas)
+        
+    #-----------------------Red neuronal Seleccionar accion-----------------------   
+    #Prueba de la red neuronal, seleccion de accion con:
+    #    4 acciones libres
+    #    7 cartas distintas a elegir
+    def test_caso_18(self):
+        utils.accionCorrecta(self.redNeuronal, mano7CartasDistintas, accionesDisponiblesTodas)
+         
+    #Prueba de la red neuronal, seleccion de accion con:
+    #    4 acciones libres
+    #    4 cartas iguales a elegir
+    def test_caso_19(self):
+        utils.accionCorrecta(self.redNeuronal, mano4Iguales, accionesDisponiblesTodas)
+         
+    #Prueba de la red neuronal, seleccion de accion con:
+    #    4 acciones libres
+    #    0 cartas a elegir        
+    def test_caso_20(self):
+        utils.accionException(self.redNeuronal, manoVacia, accionesDisponiblesTodas)
+
+    #Prueba de la red neuronal, seleccion de accion con:
+    #    Accion libre de tipo Secreto
+    #    7 cartas distintas a elegir        
+    def test_caso_21(self):
+        utils.accionCorrecta(self.redNeuronal, mano7CartasDistintas, accionesDisponiblesSecreto)
+   
+    #Prueba de la red neuronal, seleccion de accion con:
+    #    Accion libre de tipo Secreto
+    #    1 carta a elegir
+    def test_caso_22(self):
+        utils.accionCorrecta(self.redNeuronal, mano1Iguales, accionesDisponiblesSecreto)
+  
+    #Prueba de la red neuronal, seleccion de accion con:
+    #    Accion libre de tipo Renuncia
+    #    7 cartas distintas a elegir
+    def test_caso_23(self):
+        utils.accionCorrecta(self.redNeuronal, mano7CartasDistintas, accionesDisponiblesRenuncia)
+    
+    #Prueba de la red neuronal, seleccion de accion con:
+    #    Accion libre de tipo Renuncia
+    #    2 de cartas a elegir, todas iguales
+    def test_caso_24(self):
+        utils.accionCorrecta(self.redNeuronal, mano2Iguales, accionesDisponiblesRenuncia)
+   
+    #Prueba de la red neuronal, seleccion de accion con:
+    #    Accion libre de tipo Regalo
+    #    7 cartas distintas a elegir
+    def test_caso_25(self):
+        utils.accionCorrecta(self.redNeuronal, mano7CartasDistintas, accionesDisponiblesRegalo)
+
+    #Prueba de la red neuronal, seleccion de accion con:
+    #    Accion libre de tipo Regalo
+    #    3 de cartas a elegir, todas iguales
+    def test_caso_26(self):
+        utils.accionCorrecta(self.redNeuronal, mano3Iguales, accionesDisponiblesRegalo)
+ 
+    #Prueba de la red neuronal, seleccion de accion con:
+    #    Accion libre de tipo Competicion
+    #    7 cartas distintas a elegir
+    def test_caso_27(self):
+        utils.accionCorrecta(self.redNeuronal, mano7CartasDistintas, accionesDisponiblesCompeticion)
+
+    #Prueba de la red neuronal, seleccion de accion con:
+    #    Accion libre de tipo Competicion
+    #    4 de cartas a elegir, todas iguales
+    def test_caso_28(self):
+        utils.accionCorrecta(self.redNeuronal, mano4Iguales, accionesDisponiblesCompeticion)
+
+    #Prueba de la red neuronal, seleccion de accion con:
+    #    Sin acciones libres
+    def test_caso_29(self):
+        utils.accionException(self.redNeuronal, mano7CartasDistintas, accionesDisponiblesNinguna)
+        
+    #-----------------Red neuronal Seleccionar accion de seleccion-----------------'''
+    #Prueba de la red neuronal, seleccion de accion de seleccion de tipo regalo con:
+    #   Todas las cartas distintas
+    def test_caso_30(self):
+        utils.accionSeleccionCorrecta(self.redNeuronal, cartasAccionSeleccionRegalo0Iguales)
+        
+    #Prueba de la red neuronal, seleccion de accion de seleccion de tipo regalo con:
+    #   2 cartas iguales
+    def test_caso_31(self):
+        utils.accionSeleccionCorrecta(self.redNeuronal, cartasAccionSeleccionRegalo2Iguales)
+        
+    #Prueba de la red neuronal, seleccion de accion de seleccion de tipo regalo con:
+    #   Todas las cartas iguales
+    def test_caso_32(self):
+        utils.accionSeleccionCorrecta(self.redNeuronal, cartasAccionSeleccionRegalo3Iguales)
+        
+    #Prueba de la red neuronal, seleccion de accion de seleccion de tipo competicion con:
+    #   Las dos opciones iguales
+    def test_caso_33(self):
+        utils.accionSeleccionCorrecta(self.redNeuronal, cartasAccionSeleccionCompeticionIguales)
+        
+    #Prueba de la red neuronal, seleccion de accion de seleccion de tipo competicion con:
+    #   Las dos opciones distintas
+    def test_caso_34(self):
+        utils.accionSeleccionCorrecta(self.redNeuronal, cartasAccionSeleccionCompeticionDistintas)
         
     
 if __name__ == "__main__":
