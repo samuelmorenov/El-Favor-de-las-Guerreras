a6bdff0ffb875ccbdf55ceeee7e37c69c457b474
samuelmorenov <xamelsamuel@gmail.com>
2022-05-01 14:39:15 +0200

    Cambio en el modo de comentar los test para poder hacer un comentario de varias lineas de los test que no se desean probar

--- src/test/Tests.py
+++ src/test/Tests.py
@@ -32,99 +32,76 @@ class Test(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(Test, self).__init__(*args, **kwargs)
         self.bot = BotTonto("Bot de testing", 0)
-    
-    '''    
-    Prueba del bot, seleccion de accion con:
-        4 acciones libres
-        7 cartas distintas a elegir
-    '''
+        
+    #-----------------------Bot Seleccionar accion-----------------------   
+    #Prueba del bot, seleccion de accion con:
+    #    4 acciones libres
+    #    7 cartas distintas a elegir
     def test_caso_1(self):
         utils.accionCorrecta(self.bot, mano7CartasDistintas, accionesDisponiblesTodas)
-        
-    '''    
-    Prueba del bot, seleccion de accion con:
-        4 acciones libres
-        4 cartas iguales a elegir
-    '''
+         
+    #Prueba del bot, seleccion de accion con:
+    #    4 acciones libres
+    #    4 cartas iguales a elegir
     def test_caso_2(self):
         utils.accionCorrecta(self.bot, mano4Iguales, accionesDisponiblesTodas)
-        
-    '''    
-    Prueba del bot, seleccion de accion con:
-        4 acciones libres
-        0 cartas a elegir        
-    '''
+         
+    #Prueba del bot, seleccion de accion con:
+    #    4 acciones libres
+    #    0 cartas a elegir        
     def test_caso_3(self):
         utils.accionException(self.bot, manoVacia, accionesDisponiblesTodas)
 
-    '''    
-    Prueba del bot, seleccion de accion con:
-        Accion libre de tipo Secreto
-        7 cartas distintas a elegir        
-    '''
+    #Prueba del bot, seleccion de accion con:
+    #    Accion libre de tipo Secreto
+    #    7 cartas distintas a elegir        
     def test_caso_4(self):
         utils.accionCorrecta(self.bot, mano7CartasDistintas, accionesDisponiblesSecreto)
-
-    '''    
-    Prueba del bot, seleccion de accion con:
-        Accion libre de tipo Secreto
-        1 carta a elegir
-    '''
+   
+    #Prueba del bot, seleccion de accion con:
+    #    Accion libre de tipo Secreto
+    #    1 carta a elegir
     def test_caso_5(self):
         utils.accionCorrecta(self.bot, mano1Iguales, accionesDisponiblesSecreto)
-
-    '''    
-    Prueba del bot, seleccion de accion con:
-        Accion libre de tipo Renuncia
-        7 cartas distintas a elegir
-    '''
+  
+    #Prueba del bot, seleccion de accion con:
+    #    Accion libre de tipo Renuncia
+    #    7 cartas distintas a elegir
     def test_caso_6(self):
         utils.accionCorrecta(self.bot, mano7CartasDistintas, accionesDisponiblesRenuncia)
-
-    '''    
-    Prueba del bot, seleccion de accion con:
-        Accion libre de tipo Renuncia
-        2 de cartas a elegir, todas iguales
-    '''
+    
+    #Prueba del bot, seleccion de accion con:
+    #    Accion libre de tipo Renuncia
+    #    2 de cartas a elegir, todas iguales
     def test_caso_7(self):
         utils.accionCorrecta(self.bot, mano2Iguales, accionesDisponiblesRenuncia)
-
-    '''    
-    Prueba del bot, seleccion de accion con:
-        Accion libre de tipo Regalo
-        7 cartas distintas a elegir
-    '''
+   
+    #Prueba del bot, seleccion de accion con:
+    #    Accion libre de tipo Regalo
+    #    7 cartas distintas a elegir
     def test_caso_8(self):
         utils.accionCorrecta(self.bot, mano7CartasDistintas, accionesDisponiblesRegalo)
 
-    '''    
-    Prueba del bot, seleccion de accion con:
-        Accion libre de tipo Regalo
-        3 de cartas a elegir, todas iguales
-    '''
+    #Prueba del bot, seleccion de accion con:
+    #    Accion libre de tipo Regalo
+    #    3 de cartas a elegir, todas iguales
     def test_caso_9(self):
         utils.accionCorrecta(self.bot, mano3Iguales, accionesDisponiblesRegalo)
-
-    '''    
-    Prueba del bot, seleccion de accion con:
-        Accion libre de tipo Competicion
-        7 cartas distintas a elegir
-    '''
+ 
+    #Prueba del bot, seleccion de accion con:
+    #    Accion libre de tipo Competicion
+    #    7 cartas distintas a elegir
     def test_caso_10(self):
         utils.accionCorrecta(self.bot, mano7CartasDistintas, accionesDisponiblesCompeticion)
 
-    '''    
-    Prueba del bot, seleccion de accion con:
-        Accion libre de tipo Competicion
-        4 de cartas a elegir, todas iguales
-    '''
+    #Prueba del bot, seleccion de accion con:
+    #    Accion libre de tipo Competicion
+    #    4 de cartas a elegir, todas iguales
     def test_caso_11(self):
         utils.accionCorrecta(self.bot, mano4Iguales, accionesDisponiblesCompeticion)
 
-    '''    
-    Prueba del bot, seleccion de accion con:
-        Sin acciones libres
-    '''
+    #Prueba del bot, seleccion de accion con:
+    #    Sin acciones libres
     def test_caso_12(self):
         utils.accionException(self.bot, mano7CartasDistintas, accionesDisponiblesNinguna)
 
