82c0519f5f818b03c2a1f4da528f622242a8b85f
samuelmorenov <xamelsamuel@gmail.com>
2022-05-28 11:07:18 +0200

    Añadida configuracion del logger para que este habilitado por defecto

--- src/main/python/LoggerConfig.py
+++ src/main/python/LoggerConfig.py
@@ -2,20 +2,23 @@ import logging
 
 def initLogger(level):
     logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-    rootLogger = logging.getLogger()
+    logger = logging.getLogger()
     
-    while rootLogger.hasHandlers():
-        rootLogger.removeHandler(rootLogger.handlers[0])
+    while logger.hasHandlers():
+        logger.removeHandler(logger.handlers[0])
         
     if(level == None):
         return
         
-    rootLogger.setLevel(level) #INFO/DEBUG
+    logger.setLevel(level) #INFO/DEBUG
     
     fileHandler = logging.FileHandler("logfile.log")
     fileHandler.setFormatter(logFormatter)
-    rootLogger.addHandler(fileHandler)
+    logger.addHandler(fileHandler)
     
     consoleHandler = logging.StreamHandler()
     consoleHandler.setFormatter(logFormatter)
-    rootLogger.addHandler(consoleHandler)
+    logger.addHandler(consoleHandler)
+    
+    logger.disabled = False
+    
